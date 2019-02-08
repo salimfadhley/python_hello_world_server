0551865dabf7899efb24dbfe1a96c50d9b97e76f
sal <slpg500a>
2019-02-08 15:13:17 +0000

    First add

---
+++ src/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+from setuptools import setup
+import os
+
+PROJECT_ROOT, _ = os.path.split(__file__)
+REVISION = '0.0.0'
+PROJECT_NAME = 'homework'
+PROJECT_AUTHORS = "Salim Fadhley"
+PROJECT_EMAILS = 'salimfadhley@gmail.com'
+PROJECT_URL = "https://gitlab.com/salimfadhley/exodus_point_homework"
+SHORT_DESCRIPTION = 'Interview assignment'
+
+try:
+    DESCRIPTION = open(os.path.join(PROJECT_ROOT, "readme.md")).read()
+except IOError:
+    DESCRIPTION = SHORT_DESCRIPTION
+
+try:
+    REQUIREMENTS = list(open("requirements.txt").read().splitlines())
+except IOError:
+    REQUIREMENTS = []
+
+setup(
+    name=PROJECT_NAME.lower(),
+    version=REVISION,
+    author=PROJECT_AUTHORS,
+    author_email=PROJECT_EMAILS,
+    packages=['mexico'],
+    package_dir={"":"main"},
+    zip_safe=True,
+    include_package_data=False,
+    install_requires=REQUIREMENTS,
+    url=PROJECT_URL,
+    description=SHORT_DESCRIPTION,
+    long_description=DESCRIPTION,
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'flash = blink1.flash:flash',
+            'shine = blink1.shine:shine'
+        ]
+    }
+)
