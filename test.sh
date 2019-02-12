403880429a9a27b40515b5d08d6bfcd00314d9dc
sal <slpg500a>
2019-02-12 16:36:33 +0000

    Fix user permissions

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 #docker-compose run --entrypoint=find application /project
-docker-compose build
 touch junit.xml
 chmod a+rw junit.xml
 docker-compose run --entrypoint="python" application -m pytest --verbose --tb=long --junitxml=/tmp/junit.xml /project/src/tests
