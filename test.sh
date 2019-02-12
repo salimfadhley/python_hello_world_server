04aaee331c129bd7c79fbcbbc728700b2abdc8ea
sal <slpg500a>
2019-02-12 14:02:36 +0000

    Fix user permissions

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker-compose run --entrypoint=ls application -l /project
+docker-compose run --entrypoint=ls application -l /project/src
 docker-compose run --entrypoint="python" application -m pytest --verbose --tb=long --junitxml=/project/junit.xml /project/src/tests
